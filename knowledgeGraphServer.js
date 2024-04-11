4ec0fe280a29d81bddc97aeab72fca3376e1f137
youjeong Jeong <wjddbwjd1205@naver.com>
2024-04-11 17:43:03 +0900

    feat: neo4j에서 노드정보와 관계정보를 가져와서 응답을 보내주는 기능
    d3.js가 그래프를 그릴 수 있도록 하는 형태의 객체로, 노드와 관계를 저장

--- knowledgeGraphServer.js
+++ knowledgeGraphServer.js
@@ -16,6 +16,75 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../metasearch_knowledgegraph/build/index.html'))
 })
 
+//neo4j에서 데이터 가져오기
+const neo4j = require('neo4j-driver'); //npm install --save neo4j-driver
+
+// Neo4j 데이터베이스 설정
+const neo4jAddress = "bolt://113.198.85.4:80";
+const dbUserName = "neo4j";
+const dbPassword = "12345678";
+//const dbName = "youjeong";
+
+// Neo4j 데이터베이스 설정
+const driver = neo4j.driver(
+  neo4jAddress,
+  neo4j.auth.basic(dbUserName, dbPassword)
+);
+
+// 데이터를 불러오는 API 엔드포인트
+app.get('/api/graphData/:dbName', async (req, res) => {
+  const dbName = req.params.dbName; // URL에서 dbName 추출
+  console.log(dbName);
+  if (!dbName) {
+    return res.status(400).send("dbName is required");
+  }
+  const session = driver.session({ database: dbName });
+  try {
+    const result = await session.run("MATCH (n)-[r]->(m) RETURN n, r, m");
+    const nodesMap = new Map(); // 중복 노드 방지를 위한 Map
+    const links = [];
+
+    result.records.forEach((record) => {
+      const n = record.get("n");
+      const r = record.get("r");
+      const m = record.get("m");
+
+      // nodes 배열에 노드 정보 추가
+      // Map을 사용하여 중복된 노드를 추가하지 않도록 한다.
+      if (!nodesMap.has(n.identity.low)) {
+        nodesMap.set(n.identity.low, {
+          id: n.identity.low,
+          label: n.properties.name,
+          group: 0,
+        });
+      }
+
+      if (!nodesMap.has(m.identity.low)) {
+        nodesMap.set(m.identity.low, {
+          id: m.identity.low,
+          label: m.properties.name,
+          group: 1,
+        });
+      }
+
+      // links 배열에 관계 정보 추가
+      links.push({
+        source: r.end.low, //.toNumber(),
+        target: r.start.low, //.toNumber(),
+        type: r.type,
+      });
+    });
+
+    const nodes = Array.from(nodesMap.values());
+    res.json({ nodes, links });
+  } catch (error) {
+    console.error("Error fetching data from Neo4j", error);
+    res.status(500).send("Error fetching data from Neo4j");
+  } finally {
+    await session.close();
+  }
+}); 
+
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, '../metasearch_knowledgegraph/build/index.html'));
 });
