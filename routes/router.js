9231df07387b410a0c765a0a5ccee70fb77c37e5
youjeong Jeong <wjddbwjd1205@naver.com>
2024-04-16 21:45:54 +0900

    refactor: 모듈 임포트 방식 수정 및 파일 업로드 로직 분리
    - graphData 모듈의 임포트 방식을 구조 분해 할당에서 직접 할당으로 변경
      이는 graphData 모듈이 단일 함수를 export하는 구조로 변경되었기 때문임
    - 파일 업로드 처리 로직을 기존 위치에서 분리하여 fileUpload.js로 이동
    - '/upload' 경로에 대한 라우팅을 fileUploadRouter를 사용하여 처리하도록 변경
      이를 통해 코드의 모듈화 강화 및 가독성, 유지보수성 향상

--- routes/router.js
+++ routes/router.js
@@ -2,32 +2,15 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-const { upload } = require('../middlewares/fileUpload');
-const { getGraphData } = require('./graphData');
+const fileUploadRouter = require('./fileUpload');
+const getGraphData = require('./graphData');
 
 router.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../../metasearch_knowledgegraph/build/index.html'));
 });
 
-router.post('/upload', upload.single('image'), function (req, res, next) {
-    const file = req.file;
-    if (!file) {
-      return res.status(400).send('No file uploaded.');
-    }
-    
-    // MIME 타입과 확장자 검사
-    const extension = file.originalname.split('.').pop();
-    const mimeType = file.mimetype;
-    if (
-      (mimeType === 'image/jpeg' && extension !== 'jpg' && extension !== 'jpeg') ||
-      (mimeType === 'image/png' && extension !== 'png') ||
-      (mimeType === 'image/heic' && extension !== 'heic')
-    ) {
-      return res.status(400).send('File extension does not match MIME type.');
-    }
-  
-    res.send('File uploaded successfully');
-  });
+// 파일 업로드 라우터를 '/upload' 경로에 매핑합니다.
+router.use('/upload', fileUploadRouter);
 
 router.get('/api/graphData/:dbName', getGraphData);
 
