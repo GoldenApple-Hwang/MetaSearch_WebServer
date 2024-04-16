a594a1677efbc8ef7baa4d4b47551b3a2df23821
youjeong Jeong <wjddbwjd1205@naver.com>
2024-04-16 21:37:48 +0900

    refactor: 파일 업로드 유효성 검사 기능을 services 폴더로 옮김
    - 파일 존재 여부와 MIME 타입-확장자 일치 여부 검사 로직
    - 지원되지 않는 파일 형식이 업로드 시 에러 메시지 반환

---
+++ services/fileService.js
@@ -0,0 +1,22 @@
+// services/fileServices.js
+const path = require('path');
+
+function validateFile(file) {
+  if (!file) {
+    throw new Error('No file uploaded.');
+  }
+
+  const extension = file.originalname.split('.').pop();
+  const mimeType = file.mimetype;
+  if (
+    (mimeType === 'image/jpeg' && (extension !== 'jpg' && extension !== 'jpeg')) ||
+    (mimeType === 'image/png' && extension !== 'png') ||
+    (mimeType === 'image/heic' && extension !== 'heic')
+  ) {
+    throw new Error('File extension does not match MIME type.');
+  }
+
+  return file; // Return file if validation is successful
+}
+
+module.exports = { validateFile };
