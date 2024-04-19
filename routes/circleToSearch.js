3e7cbfb57356c56e395084acf88e9f52d56cf634
youjeong Jeong <wjddbwjd1205@naver.com>
2024-04-19 20:52:55 +0900

    refactor: circleToSearch 라우트 경로 간소화
    - 'circleToSearch.js' 내에서 라우트 경로를 '/'로 변경함. 상위 라우터에서 이미 '/android/circleToSearch' 경로가 지정되었기 때문에 중복을 제거

--- routes/circleToSearch.js
+++ routes/circleToSearch.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { searchPhotos } = require("../services/circleToSearchService");
 
 // '/android/circleToSearch' 경로에 대한 POST 요청 처리
-router.post("/android/circleToSearch", async (req, res) => {
+router.post("/", async (req, res) => {
   try {
     const { dbName } = req.body; // 요청 바디에서 dbName 추출
     const { properties } = req.body; // 요청 바디에서 properties 추출
@@ -18,7 +18,7 @@ router.post("/android/circleToSearch", async (req, res) => {
     if (photos.commonPhotos.length === 0 && Object.keys(photos.individualPhotos).length === 0) {
       return res.status(200).json({ message: "No photos found for the given properties." });
     }
-    
+
     res.json({ photos }); // 응답으로 사진 이름들을 JSON 형태로 반환
   } catch (error) {
     console.error("Error processing search request:", error);
