cae8ccb7eaf5e67fbfa571ddfb4d3d8092cbf217
youjeong Jeong <wjddbwjd1205@naver.com>
2024-04-24 19:57:23 +0900

    feat: CSV 파일 업로드를 위한 multer 설정 추가
    - multer storage 설정을 추가하여 사용자가 CSV 파일을 직접 업로드 할 수 있도록 함
    - 업로드된 파일의 이름은 중복을 방지하기 위해 URL 디코드된 원본 파일 이름을 사용
    - Neo4j 데이터베이스 import 폴더로 직접 파일 업로드

---
+++ middlewares/csvUploadMiddleware.js
@@ -0,0 +1,21 @@
+// middlewares/csvUpload.js
+import multer, { diskStorage } from 'multer';
+import { extname, basename } from 'path';
+
+// multer 디스크 스토리지 설정
+const storage = diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'C:/Users/hwang/.Neo4jDesktop/relate-data/dbmss/dbms-03774803-1a7b-4c49-bbce-e4aeb8c59827/import/') // CSV 파일을 저장할 경로
+    },
+    filename: function (req, file, cb) {
+        // 원본 파일 이름을 URL 디코딩하여 사용
+        const decodedFileName = decodeURIComponent(file.originalname);
+        const extension = extname(decodedFileName);
+        const filename = basename(decodedFileName, extension) + extension; // 파일 이름에 업로드 시간을 추가하여 중복 방지
+        cb(null, filename);
+    }
+});
+
+const csvUpload = multer({ storage: storage });
+
+export default csvUpload;
