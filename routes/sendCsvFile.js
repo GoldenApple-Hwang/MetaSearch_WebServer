67a59efd418f182655a93143f9ff0cdd0527dee8
youjeong Jeong <wjddbwjd1205@naver.com>
2024-04-25 20:04:01 +0900

    feat: neo4jserver/csv 경로에 대한 POST 요청 처리 추가
    - /neo4jserver/csv 경로에 대한 POST 요청을 처리하기 위한 라우터 추가
    - 요청에서 dbName을 추출하여 해당 dbName에 맞는 csv 파일의 경로를 반환하는 sendCsvFile 서비스를 호출
    - 성공적으로 파일 경로를 받으면, 클라이언트에 csv 파일을 전송
    - 에러 처리를 위해 try-catch 구문 사용하여 에러 발생 시 404 상태 코드와 함께 에러 메시지 반환

---
+++ routes/sendCsvFile.js
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import sendCsvFile from "../services/sendCsvFileService.js"; // CSV 업로드를 위한 미들웨어
+const router = Router();
+
+// '/neo4jserver/csv' 경로에 대한 POST 요청 처리
+router.post("/", async (req, res) => {
+  try {
+    const dbName = req.body.dbName; // 요청 바디에서 dbName 추출
+    console.log(`POST request to /neo4jserver/csv with ${dbName} `);
+
+    const filePath = await sendCsvFile(dbName);
+    res.sendFile(filePath);
+
+    res.json(); // 응답으로 csv 파일을 반환
+  } catch (error) {
+    res.status(404).send({ message: error.message });
+  }
+});
+
+export default router;
