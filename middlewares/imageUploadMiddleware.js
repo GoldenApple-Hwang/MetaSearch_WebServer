86bb4137ad21a940f47c91c856c959cce352154f
youjeong Jeong <wjddbwjd1205@naver.com>
2024-04-29 20:20:21 +0900

    feat: 이미지 저장 경로를 동적으로 설정하여 데이터베이스 별 이미지 관리 개선
    - 이미지 업로드 미들웨어에 데이터베이스 이름을 기반으로 한 동적 경로 설정 로직 추가.
    - 요청의 URL 쿼리에서 'dbName'을 추출하여 해당 데이터베이스 이름으로 폴더 경로를 구성.
    - 해당 폴더가 존재하지 않는 경우, 폴더를 새로 생성하여 파일 시스템의 구성을 자동화.
    - 이미지 파일은 디코드된 원본 파일 이름을 유지하면서 새로 생성된 경로에 저장되도록 설정.

--- middlewares/imageUploadMiddleware.js
+++ middlewares/imageUploadMiddleware.js
@@ -1,11 +1,22 @@
 //middlewares/imageUploadMiddleware.js
 import multer, { diskStorage } from 'multer';
 import { extname, basename } from 'path';
+import fs from 'fs';
 
 // multer 디스크 스토리지 설정
 const storage = diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'C:/Users/hwang/Documents/erica/Github/MetaSearch_WebServer/public/images')
+        // URL 쿼리에서 dbName 읽기
+        const dbName = req.query.dbName;
+        console.log(`2 image upload dbName ${dbName}`);
+        const path = `C:/Users/hwang/Documents/erica/Github/MetaSearch_WebServer/public/images/${dbName}`;
+        
+        // 폴더 존재 여부 확인 및 생성
+        if (!fs.existsSync(path)) {
+            fs.mkdirSync(path, { recursive: true }); // 'recursive: true' 옵션으로 하위 폴더까지 생성
+        }
+
+        cb(null, path)
     },
     filename: function (req, file, cb) {
         // 원본 파일 이름을 URL 디코딩하여 사용
