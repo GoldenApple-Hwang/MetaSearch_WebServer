2295a3dbd7d84b99a0e282bc5a5d3d32535f9b76
youjeong Jeong <wjddbwjd1205@naver.com>
2024-04-29 20:18:35 +0900

    feat: 이미지 업로드 라우터에서 다중 파일 업로드와 데이터베이스 이름 검증 기능 추가
    - 이미지를 dbName에 해당하는 폴더에 저장하기 위해 dbName을 받아옴
    - 이미지 업로드 미들웨어를 'upload.single'에서 'upload.fields'로 변경하여 다수의 이미지 파일을 처리할 수 있도록 개선.
    - 요청 쿼리에서 'dbName'을 검증하는 로직 추가로, 업로드 프로세스의 안정성과 데이터 무결성을 강화.

--- routes/imageUpload.js
+++ routes/imageUpload.js
@@ -1,12 +1,23 @@
 // routes/imageUpload.js
-import { Router } from 'express';
-import upload from '../middlewares/imageUploadMiddleware.js';
-import validateImage from '../services/imageService.js';
+import { Router } from "express";
+import upload from "../middlewares/imageUploadMiddleware.js";
+import validateImage from "../services/imageService.js";
 const router = Router();
 
 // 이제 이 라우터는 '/android/uploadimg'로 매핑될 때 루트 경로 '/'에서 작동합니다.
-router.post('/', upload.single('image'), (req, res, next) => {
+router.post("/", upload.fields([{ name: 'image' }]), (req, res, next) => {
   try {
+    const dbName = req.query.dbName;
+    console.log(`1 image upload dbName ${dbName}`);
+    if (!dbName) {
+      throw new Error("Database name ('source') is not provided.");
+    }
+
+    // 파일 배열에서 첫 번째 이미지 파일을 가져옵니다.
+    const imageFile = req.files['image'] ? req.files['image'][0] : null;
+    if (!imageFile) {
+      throw new Error("No image file uploaded.");
+    }
     validateImage(req.file);
     res.send("images uploaded successfully");
   } catch (error) {
