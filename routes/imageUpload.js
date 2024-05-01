c1299e72264466e53f0804772dcb6c8537dd4d6c
youjeong Jeong <wjddbwjd1205@naver.com>
2024-05-01 22:17:31 +0900

    feat: 이미지 업로드 라우트에 이미지 처리 기능 개선 및 오류 수정
    - 이미지 업로드 미들웨어에 'processImage' 기능 추가
    - 파일 업로드 확인 로직 개선, 이미지 파일 정확 처리 보장
    - validateImage 함수 호출 시 적절한 파일 객체 사용하도록 수정

--- routes/imageUpload.js
+++ routes/imageUpload.js
@@ -1,11 +1,11 @@
 // routes/imageUpload.js
 import { Router } from "express";
-import upload from "../middlewares/imageUploadMiddleware.js";
+import upload, { processImage } from "../middlewares/imageUploadMiddleware.js";
 import validateImage from "../services/imageService.js";
 const router = Router();
 
 // 이제 이 라우터는 '/android/uploadimg'로 매핑될 때 루트 경로 '/'에서 작동합니다.
-router.post("/", upload.fields([{ name: 'image' }]), (req, res, next) => {
+router.post("/", upload.fields([{ name: 'image' }]), processImage, (req, res, next) => {
   try {
     const dbName = req.query.dbName;
     console.log(`1 image upload dbName ${dbName}`);
@@ -13,12 +13,17 @@ router.post("/", upload.fields([{ name: 'image' }]), (req, res, next) => {
       throw new Error("Database name ('source') is not provided.");
     }
 
+    if (!req.files || !req.files['image'] || req.files['image'].length === 0) {
+      console.log("No file uploaded.");
+      return res.status(400).send("No file uploaded.");
+    }
+
     // 파일 배열에서 첫 번째 이미지 파일을 가져옵니다.
     const imageFile = req.files['image'] ? req.files['image'][0] : null;
     if (!imageFile) {
       throw new Error("No image file uploaded.");
     }
-    validateImage(req.file);
+    validateImage(imageFile);
     res.send("images uploaded successfully");
   } catch (error) {
     res.status(400).send(error.message);
