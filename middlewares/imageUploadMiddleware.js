5165ea3a5f940a3dbdfba900e58006503ac4801e
youjeong Jeong <wjddbwjd1205@naver.com>
2024-05-01 22:18:43 +0900

    feat: 이미지 업로드 처리를 위한 sharp 라이브러리 사용 및 메모리 스토리지 설정 추가
    - multer의 diskStorage에서 memoryStorage로 변경하여 이미지 파일 처리 방식 개선
    - sharp 라이브러리를 이용한 이미지 리사이징 및 적용 포맷 변경 기능 추가
    - 이미지 처리 로직을 담당하는 processImage 미들웨어 함수 구현

--- middlewares/imageUploadMiddleware.js
+++ middlewares/imageUploadMiddleware.js
@@ -1,32 +1,51 @@
-//middlewares/imageUploadMiddleware.js
-import multer, { diskStorage } from 'multer';
+// middlewares/imageUploadMiddleware.js
+import multer from 'multer';
 import { extname, basename } from 'path';
 import fs from 'fs';
+import sharp from 'sharp';
 
-// multer 디스크 스토리지 설정
-const storage = diskStorage({
-    destination: function (req, file, cb) {
-        // URL 쿼리에서 dbName 읽기
+// multer 메모리 스토리지 설정
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+export const processImage = async (req, res, next) => {
+    if (req.files && req.files.image && req.files.image.length > 0) { // 파일 배열 접근 수정
+        const imageFile = req.files.image[0]; // 첫 번째 이미지 파일 사용
         const dbName = req.query.dbName;
-        console.log(`2 image upload dbName ${dbName}`);
+        console.log(`Image upload dbName ${dbName}`);
         const path = `C:/Users/hwang/Documents/erica/Github/MetaSearch_WebServer/public/images/${dbName}`;
-        
+
         // 폴더 존재 여부 확인 및 생성
         if (!fs.existsSync(path)) {
-            fs.mkdirSync(path, { recursive: true }); // 'recursive: true' 옵션으로 하위 폴더까지 생성
+            fs.mkdirSync(path, { recursive: true });
         }
 
-        cb(null, path)
-    },
-    filename: function (req, file, cb) {
         // 원본 파일 이름을 URL 디코딩하여 사용
-        const decodedFileName = decodeURIComponent(file.originalname);
+        const decodedFileName = decodeURIComponent(imageFile.originalname);
         const extension = extname(decodedFileName);
         const filename = basename(decodedFileName, extension) + extension;
-        cb(null, filename);
-    }
-});
+        const outputPath = `${path}/${filename}`;
 
-const upload = multer({ storage: storage });
+        // sharp로 이미지 처리
+        try {
+            await sharp(imageFile.buffer)
+                .resize(300, 300)
+                .toFormat('jpeg')
+                .jpeg({ quality: 90 })
+                .toFile(outputPath);
+
+            req.processedImagePath = outputPath;
+            next();
+        } catch (error) {
+            console.error('Error processing image:', error);
+            res.status(500).send('Failed to process image');
+            return; // 오류 발생 시 중단
+        }
+    } else {
+        console.error('No image file uploaded.');
+        res.status(400).send('No image file uploaded.');
+        return; // 파일이 없을 경우 중단
+    }
+};
 
 export default upload;
