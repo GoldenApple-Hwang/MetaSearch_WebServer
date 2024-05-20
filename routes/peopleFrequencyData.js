607c6905021ca55dc299518323b16525cc81a054
youjeong Jeong <wjddbwjd1205@naver.com>
2024-05-20 20:33:43 +0900

    feat: 특정 인물 빈도 데이터 조회를 위한 POST 라우트 추가
    - 기존에는 데이터베이스 이름만을 이용하여 빈도 데이터를 조회하는 GET 라우트만 존재했음.
    - 새로운 POST 라우트를 통해 클라이언트가 데이터베이스 이름과 특정 인물 이름의 리스트를 제공하면,
      해당 인물들의 빈도 데이터를 조회할 수 있도록 구현.
    - 오류 발생 시 500 상태 코드와 함께 에러 메시지를 반환하도록 예외 처리를 강화.

--- routes/peopleFrequencyData.js
+++ routes/peopleFrequencyData.js
@@ -1,6 +1,6 @@
 // routes/peopleFrequencyData.js
 import { Router } from 'express';
-import fetchPeopleFrequencyData from '../services/peopleFrequencyDataService.js';
+import { fetchPeopleFrequencyData, fetchSpecificPeopleFrequencyData } from '../services/peopleFrequencyDataService.js';
 
 const router = Router();
 
@@ -11,6 +11,18 @@ router.get('/:dbName', async (req, res) => {
     res.json(data);
   } catch (error) {
     console.error(`Failed to retrieve data for ${req.params.dbName}: ${error.message}`);
+    res.status(500).send('Server Error: Unable to retrieve data');
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const { dbName, personNames } = req.body;
+    const data = await fetchSpecificPeopleFrequencyData(dbName, personNames);
+    console.log(data);
+    res.json(data);
+  } catch (error) {
+    console.error(`Failed to retrieve data for ${dbName}, ${personNames.join(', ')}: ${error.message}`);
     res.status(500).send('Server Error: Unable to retrieve data');
   }
 });
