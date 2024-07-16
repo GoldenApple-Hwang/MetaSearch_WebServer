09c01fd4470090a05e75daa0e5c207fb823dbe0e
youjeong Jeong <wjddbwjd1205@naver.com>
2024-07-16 12:55:44 +0900

    feat: 이미지 업로드 라우트에 이미지 처리 단계 추가
    - 업로드 후 이미지 처리를 위해 processImage 미들웨어 추가
    - 이미지 유효성 검사는 주석 처리하여 일시 비활성화
    - 파일 미업로드 및 데이터베이스 이름 누락 시 오류 처리 및 로깅 개선

--- routes/imageUpload.js
+++ routes/imageUpload.js
@@ -1,16 +1,21 @@
 // routes/imageUpload.js
 import { Router } from "express";
-import upload from "../middlewares/imageUploadMiddleware.js";
-import validateImage from "../services/imageService.js";
+import upload, { processImage } from "../middlewares/imageUploadMiddleware.js";
+//import validateImage from "../services/imageService.js";
 const router = Router();
 
 // 이제 이 라우터는 '/android/uploadimg'로 매핑될 때 루트 경로 '/'에서 작동합니다.
-router.post("/", upload.fields([{ name: 'image' }]), (req, res, next) => {
+router.post("/", upload.fields([{ name: 'image' }]), processImage, (req, res, next) => {
   try {
     const dbName = req.query.dbName;
     console.log(`1 image upload dbName ${dbName}`);
     if (!dbName) {
       throw new Error("Database name ('source') is not provided.");
+    }
+
+    if (!req.files || !req.files['image'] || req.files['image'].length === 0) {
+      console.log("No file uploaded.");
+      return res.status(400).send("No file uploaded.");
     }
 
     // 파일 배열에서 첫 번째 이미지 파일을 가져옵니다.
